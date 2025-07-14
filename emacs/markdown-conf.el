87bb1f4ac8af040296e28090e4126855b6443ace
James Hobson <james.hobson@student.uib.no>
2025-07-14 09:40:19 +0200

    Added emacs change

---
+++ emacs/markdown-conf.el
@@ -0,0 +1,8 @@
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
+
+(provide 'markdown-conf)
